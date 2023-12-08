04cae01ae636e1e7e1e3b4ab2813a9ee70d4b36f
Knight00 <85784853+7310038081@users.noreply.github.com>
2023-12-08 11:38:23 +0000

    Datatypes in javascript

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all java code as newer version
+
+// number of datatypes
+
+/*
+1. number RANGE 2 to power 52
+2. string ""
+3. bigint
+4. boolean true/false
+5. null -> standalone value  // null is an object
+6. Undefined
+7. symbol -> unique
+
+//object
+
+*/
+
+console.log(typeof "Vivek")
