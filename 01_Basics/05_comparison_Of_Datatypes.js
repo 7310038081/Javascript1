0c2b729f6cfa40e4a5779f20e5d183c5d1b20f90
Knight00 <85784853+7310038081@users.noreply.github.com>
2023-12-08 13:13:32 +0000

    comparion of datatypes

---
+++ 01_Basics/05_comparison_Of_Datatypes.js
@@ -0,0 +1,23 @@
+// console.log(2>1) >
+// console.log(2>=1) >=
+// console.log(2<1) <
+// console.log(2<=1) <= 
+// console.log(2==1) == 
+// console.log(2!=1) !=
+
+/*
+console.log(null>0);   
+console.log(null ==0);
+console.log(null >= 0);      
+
+console.log(undefined >0);  // always avoid these kind of conversions
+console.log(undefined ==0);
+console.log(undefined >=0);
+
+
+*/
+
+// === na ki values check karta hai baliki datatypes ko bhi check krta hai
+
+console.log("2"==2);
+console.log("2" === 2);
