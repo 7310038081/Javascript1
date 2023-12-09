bf64373a0d5dc084f0cfdb2bae69c6a939ed4dfd
Knight00 <85784853+7310038081@users.noreply.github.com>
2023-12-09 07:46:37 +0000

    Number and Maths

---
+++ 01_Basics/08_numberAndmaths.js
@@ -0,0 +1,30 @@
+//Numbers
+
+
+// const Num = 400;
+// console.log(Num)
+
+// const Newnum = new Number(100)
+// console.log(Newnum)
+
+// console.log(Newnum.toString().length);  // the number changes to string .toString() method and it has some properties of string
+// console.log(Newnum.toFixed(2));  // gives the precision value of the number
+
+// const decimal = 123.892;
+// console.log(decimal.toPrecision(3));
+
+// const ner = 1000000;
+// console.log(ner.toLocaleString('en-IN')); // insert commas in the big values
+
+
+// +++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(Math);  //Math itself an object
+console.log(Math.abs(-4)); // .abs() method only changes -ve to +ve // not changes +ve to -ve
+console.log(Math.round(4.9)); //it round -off the value
+
+console.log(Math.min(3,4,5,6,7,8,9)) //gives the min value
+console.log(Math.max(3,4,5,6,7,8,9))  //gives the max value
+
+console.log(Math.random()); //read about this 
