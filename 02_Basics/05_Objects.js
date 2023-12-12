c3f113f32e8ec20e82b66c7e443f29a927a048e3
Knight00 <85784853+7310038081@users.noreply.github.com>
2023-12-12 15:00:48 +0000

    Objects,Json

---
+++ 02_Basics/05_Objects.js
@@ -0,0 +1,23 @@
+const course = {
+    courseName : "Js-tutorials",
+    coursePrice : 9999,
+    courseDate : "15-August",
+}
+
+const {courseName:Cn} = course  // De-structring  of objects
+console.log(Cn);
+
+// {
+//     "name": "Vivek",
+//     "section" :"k20uf",  //JSON
+// }
+
+[
+    {
+                  //JSON-array
+    }
+]
+
+
+//Object-destructruing
+//json,arrays
