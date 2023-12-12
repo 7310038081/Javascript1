f2a841a557ed4c5da3299cbcbfc81a8b08f12b67
Knight00 <85784853+7310038081@users.noreply.github.com>
2023-12-12 13:47:18 +0000

    Objects1

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,42 @@
+// //singleton
+// // Object.create   constructor method
+
+// //object literals
+
+const mySym = Symbol("AMAN");
+const aman  = {
+    name : "vivek",
+    [mySym]: "AMAN",
+    "full" : "vivek singh jatt",
+    email: "vs292713@gmail.com",
+    college : "lpu",
+    section: "k20uf",
+    rollNO : 25,
+}
+
+// // to access the object 
+
+// // console.log(aman.name);
+// // // console.log(aman."full");
+
+// // // // so we use the another method to access the object
+
+// // // console.log(aman["full"]);
+// // console.log(aman[mySym]);  // printing the symbol
+// // console.log(typeof mySym);
+
+// // aman.email = "singhaman5288@gmail.com"
+// // Object.freeze(aman);  // it freezes the object after that we can't change the values 
+// // console.log(aman);
+
+
+aman.fun1 = function() {
+    console.log("My name is Vivek");
+}
+
+aman.fun2 = function() {
+    console.log(`My roll no is, ${this.rollNO}`);
+}
+
+console.log(aman.fun1());
+console.log(aman.fun2());
