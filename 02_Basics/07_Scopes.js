d637cae9228e8700d4e1e00d7bfddbe8d9f9c3e8
Knight00 <85784853+7310038081@users.noreply.github.com>
2023-12-13 07:52:28 +0000

    About_Scopes

---
+++ 02_Basics/07_Scopes.js
@@ -0,0 +1,15 @@
+function one(){
+    username = "Vivek";
+
+    function two() {
+        course = "js";
+        console.log(username);
+    }
+
+    // console.log(course);
+    two();
+}
+
+one();  
+
+//two types of scopes block scope and global scope we access the global scope from anywhere in my code
