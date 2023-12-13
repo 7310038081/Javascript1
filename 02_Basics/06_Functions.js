ed97334aecc3aa054d5b8ef55b08f9e6129f8d65
Knight00 <85784853+7310038081@users.noreply.github.com>
2023-12-13 07:09:50 +0000

    Functions

--- 02_Basics/06_Functions.js
+++ 02_Basics/06_Functions.js
@@ -1,25 +1,59 @@
-// function addTwoNumbers(number1 ,number2){  //parameters
-//     return number1 + number2;
+// // function addTwoNumbers(number1 ,number2){  //parameters
+// //     return number1 + number2;
+// // }
+
+// // const result = addTwoNumbers(3,4);  //arguments
+// // console.log(result);
+
+
+// function UserLogin(username){
+//     return ` ${username}, just login`
 // }
 
-// const result = addTwoNumbers(3,4);  //arguments
-// console.log(result);
+// console.log(UserLogin("Vivek"));
+
+// function sayDigit(digit){
+
+//     if(digit===undefined){
+//         console.log("please enter the digit");
+//     }
+//     else {
+//         return  `${++digit},"this is digit"`;
+//     }
+
+// }
+// console.log(sayDigit());
 
 
-function UserLogin(username){
-    return ` ${username}, just login`
+
+// use of rest operator
+
+function calculatePrice(...digit){
+        return digit;
 }
 
-console.log(UserLogin("Vivek"));
+console.log(calculatePrice(500,600,700,800,900,10000));
 
-function sayDigit(digit){
 
-    if(digit===undefined){
-        console.log("please enter the digit");
-    }
-    else {
-        return  `${++digit},"this is digit"`;
-    }
+// const user = {
+//     username : "Vivek",
+//     price : 300,
+// }
 
+function Acc(anyObject){
+    console.log(`name is ${anyObject.username}, price is ${anyObject.price}`);
 }
-console.log(sayDigit());
+
+Acc({
+    username:"Vivek",
+    price:300
+});
+
+
+const Array1 = [100,200,300,400];
+
+function getSecond(amray){
+    return amray[1]
+}
+
+console.log(getSecond(Array1));
